9af3980254c9bd22aac74bf230c679d93967e992
Ian Frost <inf5000@gmail.com>
2026-02-12 17:13:11 -0500

    Tighten up designs

--- components/page-header/page-header.js
+++ components/page-header/page-header.js
@@ -1,10 +1,12 @@
 import { LitElement, html } from "lit";
 import styles from "./page-header.css" with { type: "css" };
 import "../heading/heading.js";
+import "../text-passage/text-passage.js";
 
 class MyPageHeader extends LitElement {
 	static properties = {
 		heading: { type: String },
+		description: { type: String },
 	};
 
 	static styles = [styles];
@@ -12,12 +14,18 @@ class MyPageHeader extends LitElement {
 	constructor() {
 		super();
 		this.heading = "";
+		this.description = false;
 	}
 
 	render() {
 		return html`
 			<div class="page-header">
 				<my-heading tagName="h1" variant="title-lg">${this.heading}</my-heading>
+				${this.description
+					? html`<my-text-passage class="page-header__description"
+							>${this.description}</my-text-passage
+						>`
+					: ""}
 			</div>
 		`;
 	}
