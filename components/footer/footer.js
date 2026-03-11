55c7f07adc2d9d7a4bc732b05ebd085d93758587
Brad Frost <brad@bradfrost.com>
2026-03-11 09:50:42 -0400

    Clean up imports

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -21,7 +21,11 @@ class MyFooter extends LitElement {
 					</ul>
 				</nav>
 				<p class="footer__copyright">
+<<<<<<< Updated upstream
 					&copy; 2026 My Company. All rights reserved.
+=======
+					&copy; 2026 Our Company. All rights reserved.
+>>>>>>> Stashed changes
 				</p>
 			</footer>
 		`;
